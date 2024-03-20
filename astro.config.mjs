5cc70c6d90bbe951aee62c516d93adc983775d69
JoseDiegoMarinContreras <DIEGOmai1995@hotmail.com>
2024-03-20 15:03:59 -0700

    Added github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://JoseDiegoMarinContreras.github.io',
+    base: '/portfolio-web'
+});
